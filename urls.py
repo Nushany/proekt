fd5195a16b856c50e3cb8a2b25ac2f733708bf39
prob <136353641+Nushany@users.noreply.github.com>
2025-04-10 18:16:12 +0500

    Update urls.py

--- urls.py
+++ urls.py
@@ -2,12 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-#    path('index/', views.index, name='home'),
     path('', views.index, name='home'),
-    path('/shopapp/biology/', views.biology),
-    path('/shopapp/chemistry/', views.chemistry),
-    path('/shopapp/signup/', views.signup, name='signup'),
-    path('/shopapp/list/', views.list, name='list'),
-    path('/shopapp/profile/', views.profile, name='profile'),
-    path('/shopapp/info/', views.info, name='info')
+    path('./biology/', views.biology),
+    path('./chemistry/', views.chemistry),
+    path('./signup/', views.signup, name='signup'),
+    path('./list/', views.list, name='list'),
+    path('./profile/', views.profile, name='profile'),
+    path('./info/', views.info, name='info')
 ]
